fab7da7f8e979a91d6fea3c3c228ca2c7dc674b1
abhishek9111 <42829419+Abhishek9111@users.noreply.github.com>
2024-08-25 10:45:34 +0530

    fix: add condition to handle dynamic echo commands inputs

--- app/main.js
+++ app/main.js
@@ -9,7 +9,18 @@ const server = net.createServer((connection) => {
   console.log("connection");
   //   connection.write("+PONG\r\n");
   connection.on("data", (data) => {
-    connection.write("+PONG\r\n");
+    // connection.write("+PONG\r\n");
+    //initial data is encoded string -> [72, 101, 108, 108, 111] which we are parsing
+    const parsedData = Buffer.from(data).toString().split("\r\n");
+    // *2\r\n $5 \r\n ECHO \r\n $3 \r\n hey \r\n
+
+    if (parsedData[2] == "ECHO") {
+      let returnString = parsedData[4];
+      let returnStringLen = returnString.len;
+      return connection.write(
+        "$" + returnStringLen + "\r\n" + returnString + "\r\n"
+      );
+    }
   });
 });
 
